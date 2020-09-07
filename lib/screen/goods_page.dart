69142fe58d17c8a3dbe258d645db302d005ee5f2
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-08 00:03:06 +0300

    Implement goods page

--- lib/screen/goods_page.dart
+++ lib/screen/goods_page.dart
@@ -12,9 +12,28 @@ class GoodsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Интернет магазин" + goods.model),
+        title: Text(goods.model),
         centerTitle: true,
+      ),
+      body: ListView(
+        children :[
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            height: 200,
+            child: Image(image: NetworkImage(goods.img),),
+          ),
+          Center(child: Text(goods.model, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),)),
+          SizedBox(height: 20,),
+          Text("Описание: ", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+          Text( goods.description, style: TextStyle(fontSize: 30),),
+          SizedBox(height: 20,),
+          Text("Цена: ", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+          Text( goods.price, style: TextStyle(fontSize: 30),),
+          SizedBox(height: 20,)
+        ]
       ),
     );
   }
