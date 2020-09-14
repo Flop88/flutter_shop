8a243c6ab9852ee31555f51c53e3ac778e48f8d9
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-14 22:43:31 +0300

    Implement json parsing from site

--- lib/screen/goods_page.dart
+++ lib/screen/goods_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/model/goods.dart';
 
-import 'cart_screen.dart';
 
 class goods_page extends StatelessWidget {
-  goods_page({ Key key,@required this.goods, this.valueSetter, this.cart}) : super(key: key);
+  goods_page({Key key, @required this.goods, this.valueSetter, this.cart})
+      : super(key: key);
 
   final Goods goods;
   final ValueSetter valueSetter;
@@ -12,49 +12,81 @@ class goods_page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(goods.model),
         centerTitle: true,
       ),
-      body: ListView(
-          children :[
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              height: 200,
-              child: Image(image: NetworkImage(goods.img),),
-            ),
-            Center(child: Text(goods.model, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),)),
-            SizedBox(height: 20,),
-            Text("Описание: ", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
-            Text( goods.description, style: TextStyle(fontSize: 30),),
-            SizedBox(height: 20,),
-            Text("Цена: ", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
-            Text( goods.price, style: TextStyle(fontSize: 30),),
-            SizedBox(height: 20,),
-            InkWell(
-              onTap: () {
-                valueSetter(goods);
-              },
-              child: Container(
-                height: 60,
-                color: Colors.lightBlue,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.add_shopping_cart, size: 50, color: Colors.white,),
-                    SizedBox(width: 10,),
-                    Text("Купить",style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),)
-                  ],
+      body: ListView(children: [
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          height: 200,
+          child: Image(
+            image: NetworkImage(goods.img),
+          ),
+        ),
+        Center(
+            child: Text(
+          goods.model,
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        )),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Описание: ",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        Text(
+          goods.description,
+          style: TextStyle(fontSize: 30),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Цена: ",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        Text(
+          goods.price,
+          style: TextStyle(fontSize: 30),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        InkWell(
+          onTap: () {
+            valueSetter(goods);
+          },
+          child: Container(
+            height: 60,
+            color: Colors.lightBlue,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.add_shopping_cart,
+                  size: 50,
+                  color: Colors.white,
                 ),
-              ),
-            )
-          ]
-      ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Купить",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
-
--- lib/screen/goods_screen.dart
+++ lib/screen/goods_screen.dart
@@ -2,15 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shop/components/goods_card.dart';
 import 'package:flutter_shop/model/goods.dart';
 import 'package:flutter_shop/screen/cart_screen.dart';
-
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class GoodsScreen extends StatefulWidget {
-
   @override
   _GoodsScreenState createState() => _GoodsScreenState();
 }
 
 class _GoodsScreenState extends State<GoodsScreen> {
+  Future<List<Goods>> _getGoods() async {
+    var data = await http.get("http://mvlikhachev.ru/flutter_shop/goods.json");
+    var jsonData = json.decode(data.body);
+
+    List<Goods> goods = [];
+    for (var g in jsonData) {
+      Goods jsonGoods = Goods(
+          id: g["id"],
+          brand: g["brand"],
+          model: g["model"],
+          type: g["type"],
+          img: g["img"],
+          price: g["price"],
+          description: g["description"]);
+      goods.add(jsonGoods);
+    }
+
+    return goods;
+  }
 
   List<Goods> cart = [];
 
@@ -23,33 +43,44 @@ class _GoodsScreenState extends State<GoodsScreen> {
         actions: [
           IconButton(
             icon: Icon(Icons.shopping_cart),
-
             onPressed: () => {
-              cart.length != 0 ? Navigator.push(context, MaterialPageRoute(builder: (context) => Cart(cart: cart))) : null // Потом можно добавить toast или еще что
+              cart.length != 0
+                  ? Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Cart(cart: cart)))
+                  : null // Потом можно добавить toast или еще что
             },
           )
         ],
         centerTitle: true,
       ),
       body: Container(
-        child: new FutureBuilder(builder: (context, snapshot) {
-
-          final goods = goodsFromJson(snapshot.data.toString());
-
-          return ListView.builder(
-            physics: BouncingScrollPhysics(),
-            itemCount: goods.length,
-            itemBuilder: (context, index) {
-              return GoodsCard(goods: goods[index], valueSetter: (selectedProduct) {
-                setState(() {
-                  cart.add(goods[index]);
-                });
-              }, cart: cart,);
-            },
-          );
-        },
-            future: DefaultAssetBundle.of(context).loadString("assets/goods.json"),
-      )
+        child: FutureBuilder(
+          future: _getGoods(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: Text("Loading..."),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return GoodsCard(
+                    goods: snapshot.data[index],
+                    valueSetter: (selectedProduct) {
+                      setState(() {
+                        cart.add(snapshot.data[index]);
+                      });
+                    },
+                    cart: cart,
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
