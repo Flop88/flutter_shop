6d639be74f03ed15e359f14814c8f6df2b5c2749
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-18 11:32:33 +0300

    Закончил на реализации вывода окончательной суммы в корзине

--- lib/screen/cart_screen.dart
+++ lib/screen/cart_screen.dart
@@ -2,78 +2,103 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shop/model/goods.dart';
 
 class Cart extends StatefulWidget {
-
   final List<Goods> cart;
-  int sum = 0;
+  static int sum = 0;
 
-  Cart({this.cart, this.sum});
+  Cart({this.cart});
+
 
   @override
   _CartState createState() => _CartState();
 }
 
 class _CartState extends State<Cart> {
+  int cartSum = 0;
+  // get sum => cartSum;
+
 
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      // widget.cart.forEach((element) {
+      //   print("length: ${cartSum + int.parse(element.price)}");
+      // });
 
-    int sum = 0;
-    // initState();
-    // print("Sum: ${widget.sum}");
+      // for(final i in widget.cart) {
+      //   print("length: ${cartSum + int.parse(i.price)}");
+      //   print("Sum: ${cartSum + int.parse(i.price)}");
+      // }
+    });
     return Scaffold(
         appBar: AppBar(
           title: Text("Корзина"),
           centerTitle: true,
         ),
-        body: Wrap(
-          children: [
-            ListView.builder(
-                shrinkWrap: true,
-                physics: BouncingScrollPhysics(),
-                itemCount: widget.cart.length,
-                itemBuilder: (context, index) {
-                  sum = sum + int.parse(widget.cart[index].price);
-                  return widget.cart.length == 0 ? Text("123") /* Что-то хотел сюда запихнуть, но забыл что. Как вспомню - доделаю */ : Card(
-                   elevation: 2.0,
-                   margin: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                   child: ListTile(
-                     title: Wrap(
-                       children: [
-                         Text(widget.cart[index].type + " ", style: TextStyle(
-                             color: Colors.black, fontWeight: FontWeight.bold),),
-                         Text(widget.cart[index].brand.toString() + " " + widget.cart[index].model),
-                       ],
-                     ),
-                     subtitle: Text("Цена: " + widget.cart[index].price + " ",
-                         style: TextStyle(
-                             color: Colors.black, fontWeight: FontWeight.bold)),
-                     leading: Image(
-                       height: 50,
-                       width: 50,
-                       image: NetworkImage(widget.cart[index].img),
-                     ),
-                     trailing: IconButton(icon: Icon(Icons.remove_shopping_cart), onPressed: () {
-                       setState(() {
-                         widget.cart.remove(widget.cart[index]);
-                       });
-                     },),
-                   ),
-                    );
-                }
-            ),
-            Divider(),
-            Text("Итого: $sum", style: TextStyle(fontSize: 24, color: Colors.black, fontWeight: FontWeight.bold),)
-          ],
-        )
-    );
+        body: ListView(children: [
+          Wrap(
+            children: [
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: BouncingScrollPhysics(),
+                  itemCount: widget.cart.length,
+                  itemBuilder: (context, index) {
+                      cartSum += int.parse(widget.cart[index].price);
+                    return widget.cart.length == 0
+                        ? Text(
+                            "123") /* Что-то хотел сюда запихнуть, но забыл что. Как вспомню - доделаю */
+                        : Card(
+                            elevation: 2.0,
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 8),
+                            child: ListTile(
+                              title: Wrap(
+                                children: [
+                                  Text(
+                                    widget.cart[index].type + " ",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(widget.cart[index].brand.toString() +
+                                      " " +
+                                      widget.cart[index].model),
+                                ],
+                              ),
+                              subtitle: Text(
+                                  "Цена: " + widget.cart[index].price + " ",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold)),
+                              leading: Image(
+                                height: 50,
+                                width: 50,
+                                image: NetworkImage(widget.cart[index].img),
+                              ),
+                              trailing: IconButton(
+                                icon: Icon(Icons.remove_shopping_cart),
+                                onPressed: () {
+                                  setState(() {
+                                    // cartSum -= int.parse(widget.cart[index].price);
+                                    widget.cart.remove(widget.cart[index]);
+                                  });
+                                },
+                              ),
+                            ),
+                          );
+                  }),
+              Divider(),
+              Text(
+                "Итого: $cartSum",
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,),
+              )
+            ],
+          ),
+        ]));
   }
 
-// @override
-// void initState() {
-//   for(int i = 0; i < widget.cart.length; i++) {
-//     print(int.parse(widget.cart[i].price));
-//     widget.sum = widget.sum + int.parse(widget.cart[i].price);
-//     print("Widget.sum: ${widget.sum}");
-//   }
-// }
+  
 }
+
