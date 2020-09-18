4ac386a4f5e81f42e3f76f3925a4a714ae932d17
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-18 23:05:34 +0300

    implement goods counter in cart

--- lib/screen/cart_screen.dart
+++ lib/screen/cart_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/model/goods.dart';
 
@@ -21,12 +23,34 @@ class _CartState extends State<Cart> {
 
   @override
   Widget build(BuildContext context) {
+    var map = Map();
+    var list = List();
+
+    final goodsCounts = map.values.toList();
+    // List<Goods> cloneCart = []..addAll(widget.cart);
+    List<Goods> result = LinkedHashSet<Goods>.from(widget.cart).toList();
+    // print(result[0].model); // => ["a", "b", "c", "d"]
+
     setState(() {
       widget.sum = 0;
+
       widget.cart.forEach((element) {
-      widget.sum = widget.sum + int.parse(element.price);
-      print(widget.sum);
+        widget.sum = widget.sum + int.parse(element.price);
+        // print(element.model);
+        // print(widget.sum);
+
+        if(!map.containsKey(element)) {
+          map[element] = 1;
+        } else {
+          map[element] +=1;
+
+        }
     });
+
+      list = map.values.toList();
+      print("Всего объектов: " + widget.cart.length.toString());
+      print("Уникальных объектов: " + list.length.toString());
+      print("В новом списке объектов: " + result.length.toString());
     });
     return Scaffold(
         appBar: AppBar(
@@ -39,54 +63,105 @@ class _CartState extends State<Cart> {
               ListView.builder(
                   shrinkWrap: true,
                   physics: BouncingScrollPhysics(),
-                  itemCount: widget.cart.length,
+                  itemCount: result.length,
                   itemBuilder: (context, index) {
                     return Card(
-                            elevation: 2.0,
-                            margin: EdgeInsets.symmetric(
-                                horizontal: 8, vertical: 8),
-                            child: ListTile(
-                              title: Wrap(
-                                children: [
-                                  Text(
-                                    widget.cart[index].type + " ",
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(widget.cart[index].brand.toString() +
-                                      " " +
-                                      widget.cart[index].model),
-                                ],
-                              ),
-                              subtitle: Text(
-                                  "Цена: " + widget.cart[index].price + " ",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold)),
-                              leading: Wrap(children: [
-                                Container(
-                                    child: Text( "${widget.count} x", style: TextStyle(fontWeight: FontWeight.bold), ),
-                                  margin: EdgeInsets.only(top: 15),
-                                ),
-                                Image(
-                                  height: 50,
-                                  width: 50,
-                                  image: NetworkImage(widget.cart[index].img),
-                                ),
-                              ]),
-                              trailing: IconButton(
-                                icon: Icon(Icons.remove_shopping_cart),
-                                onPressed: () {
-                                  setState(() {
-                                    widget.sum -= int.parse(widget.cart[index].price);
-                                    widget.cart.remove(widget.cart[index]);
-                                  });
-                                },
-                              ),
+                      elevation: 2.0,
+                      margin: EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 8),
+                      child: ListTile(
+                        title: Wrap(
+                          children: [
+                            Text(
+                             result[index].type + " ",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
                             ),
-                          );
+                            Text(result[index].brand.toString() +
+                                " " +
+                                result[index].model),
+                          ],
+                        ),
+                        subtitle: Text(
+                            "Цена: " + result[index].price + " ",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                        leading: Wrap(children: [
+                          Container(
+                            child: Text( "${list[index]} x", style: TextStyle(fontWeight: FontWeight.bold), ),
+                            margin: EdgeInsets.only(top: 15),
+                          ),
+                          Image(
+                            height: 50,
+                            width: 50,
+                            image: NetworkImage(result[index].img),
+                          ),
+                        ]),
+                        trailing: IconButton(
+                          icon: Icon(Icons.remove_shopping_cart),
+                          onPressed: () {
+                            setState(() {
+                              widget.sum -= int.parse(widget.cart[index].price);
+                              widget.cart.remove(widget.cart[index]);
+                            });
+                          },
+                        ),
+                      ),
+                    );
                   }),
+              // ListView.builder(
+              //     shrinkWrap: true,
+              //     physics: BouncingScrollPhysics(),
+              //     itemCount: widget.cart.length,
+              //     itemBuilder: (context, index) {
+              //       return Card(
+              //               elevation: 2.0,
+              //               margin: EdgeInsets.symmetric(
+              //                   horizontal: 8, vertical: 8),
+              //               child: ListTile(
+              //                 title: Wrap(
+              //                   children: [
+              //                     Text(
+              //                       widget.cart[index].type + " ",
+              //                       style: TextStyle(
+              //                           color: Colors.black,
+              //                           fontWeight: FontWeight.bold),
+              //                     ),
+              //                     Text(widget.cart[index].brand.toString() +
+              //                         " " +
+              //                         widget.cart[index].model),
+              //                   ],
+              //                 ),
+              //                 subtitle: Text(
+              //                     "Цена: " + widget.cart[index].price + " ",
+              //                     style: TextStyle(
+              //                         color: Colors.black,
+              //                         fontWeight: FontWeight.bold)),
+              //                 leading: Wrap(children: [
+              //                   Container(
+              //                       child: Text( "${widget.count} x", style: TextStyle(fontWeight: FontWeight.bold), ),
+              //                     margin: EdgeInsets.only(top: 15),
+              //                   ),
+              //                   Image(
+              //                     height: 50,
+              //                     width: 50,
+              //                     image: NetworkImage(widget.cart[index].img),
+              //                   ),
+              //                 ]),
+              //                 trailing: IconButton(
+              //                   icon: Icon(Icons.remove_shopping_cart),
+              //                   onPressed: () {
+              //                     setState(() {
+              //                       widget.sum -= int.parse(widget.cart[index].price);
+              //                       widget.cart.remove(widget.cart[index]);
+              //                     });
+              //                   },
+              //                 ),
+              //               ),
+              //             );
+              //     }),
               Divider(),
               Text(
                 "Итого: ${widget.sum}",
